c100b3ee4ac70d68e2f45e43fc17e86fb9fc17a8
gita sonia <gitasonia98@gmail.com>
2019-06-20 00:37:57 +0700

    masterblade yield section

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -1,21 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title> </title>
-	<!--link ini diambil dari css di getboostratp.com , ini untk css-->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<body>
-
-
-
-<div class="container"> 
+@extends('layouts.master') <!--menarik dari file master-->
+@section('content') <!--harus sama dg isi yield di master-->
 	@if(session('sukses'))
 	<div class="alert alert-success" role="alert">
 	{{session('sukses')}}
 	</div>
 	@endif
-<div class="row">
+	<div class="row">
 	<div class ="col-6">
 	<h1>DATA SISWA </h1>
 	</div>
@@ -107,13 +97,12 @@
       </div>
     </div>
   </div>
-</div>
+
  
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloud flare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 
 
 </body>
 </html>
 
+@endsection
