52a0677ff8421272e2ead764f6a48317d7414f01
gita sonia <gitasonia98@gmail.com>
2019-06-23 23:09:20 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,21 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// POST itu buat submit
+
+Route::get('/', function () { 
+    return view('home');
 }); 
 
+Route::get('/login','AuthController@login')->name('login');
+Route::post('/postlogin','AuthController@postlogin');
+Route::get('/logout','AuthController@logout');
+Route::group(['middleware' => 'auth'],function(){
+Route::get('/dashboard','DashboardController@index');
 Route::get('/siswa','SiswaController@index');
 Route::post('/siswa/create', 'SiswaController@create'); //untuk submit form pake method post"
 Route::get('/siswa/{id}/edit','SiswaController@edit');
 Route::post('/siswa/{id}/update','SiswaController@update'); //update buat di controller
 Route::get('/siswa/{id}/delete','SiswaController@delete');
+
+});
